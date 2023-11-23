887b0b6f8d2489bd12bf155b69453288b019a173
Anymoose98 <daniel.digiacomo98@gmail.com>
2023-11-23 21:32:58 +0100

    esercizio finito

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -1,19 +1,32 @@
-// numeri casuali tra 1 e 6
-let numero_giocatore = Math.floor(Math.random() * 6) + 1;
-let numero_pc = Math.floor(Math.random() * 6) + 1;
 
-// confrontare i risultati
-if(numero_giocatore < numero_pc){
-    let risultato = "Hai vinto!!"
-    console.log(risultato)
-}
+const button = document.querySelector("button");
+// bottone
+button.addEventListener("click", function(){
 
-else if (numero_giocatore > numero_pc){
-    let risultato = "Hai perso!!"
-    console.log(risultato)
-}
+    // numeri casuali tra 1 e 6
+    let numero_giocatore = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("giocatore").textContent = numero_giocatore
 
-else{
-    let risultato = "Pareggio"
-    console.log(risultato)
-}
+    let numero_pc = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("pc").textContent = numero_pc
+
+
+
+    // confrontare i risultati
+    if(numero_giocatore > numero_pc){
+        let risultato = "Hai vinto!!"
+        console.log(risultato)
+        document.getElementById("risultato").textContent = risultato
+    }
+
+    else if (numero_giocatore < numero_pc){
+        let risultato = "Hai perso!!"
+        console.log(risultato)
+        document.getElementById("risultato").textContent = risultato
+    }
+
+    else{
+        let risultato = "Pareggio"
+        console.log(risultato)
+        document.getElementById("risultato").textContent = risultato
+    }})
